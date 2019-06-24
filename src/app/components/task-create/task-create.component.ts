6ef41555a48a6edba9bffa96cfd0bcbbeab30d7c
OleksandrMisiuk <santa.misuklord@gmail.com>
2019-06-25 00:10:40 +0300

    Added handling exception from response in signin, signup components

--- src/app/components/task-create/task-create.component.ts
+++ src/app/components/task-create/task-create.component.ts
@@ -6,33 +6,11 @@ import { ActivatedRoute } from "@angular/router";
 import { Task } from 'src/app/model/Task';
 import { User } from 'src/app/model/User';
 import { UserService } from 'src/app/services/user.service';
-import {MomentDateAdapter} from '@angular/material-moment-adapter';
-import {DateAdapter, MAT_DATE_FORMATS, MAT_DATE_LOCALE} from '@angular/material/core';
-import * as moment from 'moment';
-import { DateRange } from '@uiowa/date-range-picker';
-
-export const MY_FORMATS = {
-  parse: {
-    dateInput: 'YYYY MMMM DDDD',
-  },
-  display: {
-    dateInput: 'LL',
-    monthYearLabel: 'MMM YYYY',
-    dateA11yLabel: 'LL',
-    monthYearA11yLabel: 'MMMM YYYY',
-  },
-};
-
 
 @Component({
   selector: 'app-task-create',
   templateUrl: './task-create.component.html',
-  styleUrls: ['./task-create.component.scss'],
-  // providers: [
-  // {provide: DateAdapter, useClass: MomentDateAdapter, deps: [MAT_DATE_LOCALE]},
-
-  //   {provide: MAT_DATE_FORMATS, useValue: MY_FORMATS},
-  // ],
+  styleUrls: ['./task-create.component.scss']
 })
 
 export class TaskCreateComponent implements OnInit {
